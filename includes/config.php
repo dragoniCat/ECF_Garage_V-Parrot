e247b60616a64d554557389ba3ea6c93d58e5c6a
dragoniCat <92878236+dragoniCat@users.noreply.github.com>
2024-02-18 19:18:51 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,6 +6,7 @@ ini_set('session.use_strict_mode', 1);
 session_set_cookie_params([
     'lifetime' => 1800,
     'domain' => 'ecf-v-parrot-59b9dd8ce789.herokuapp.com',
+    // 'domain' => 'localhost',
     'path' => '/',
     'secure' => true,
     'httponly' => true
