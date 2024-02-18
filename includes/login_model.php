c542c57df464a015ec7e9f9ebfb092f622f6b57d
dragoniCat <92878236+dragoniCat@users.noreply.github.com>
2024-02-18 17:45:23 +0100

    Update login_model.php

--- includes/login_model.php
+++ includes/login_model.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 function get_email(string $email) {
     require_once "dbh.inc.php";
-    $query = "SELECT * FROM {$DATABASE}.utilisateurs WHERE email = :email;";
+    $query = "SELECT * FROM qui5aafu163l1ogs.utilisateurs WHERE email = :email;";
     $stmt = db()->prepare($query);
     $stmt->bindParam(":email", $email);
     $stmt->execute();
