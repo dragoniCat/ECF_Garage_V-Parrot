5579067d2b8ecca0f5590a1a702177b825659b86
dragoniCat <92878236+dragoniCat@users.noreply.github.com>
2024-02-18 17:32:49 +0100

    test sql

--- includes/login_model.php
+++ includes/login_model.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 function get_email(string $email) {
-
-    $query = "SELECT * FROM '".$DATABASE."'.utilisateurs WHERE email = :email;";
+    $tableName = "utilisateurs";
+    $query = "SELECT * FROM ".$DATABASE.".".$tableName." WHERE email = :email;";
     $stmt = db()->prepare($query);
     $stmt->bindParam(":email", $email);
     $stmt->execute();
